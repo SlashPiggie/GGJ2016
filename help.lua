5a173ea625edf1bee458f5cb4b5a9be35d87544e
Binh Bui <baobinh.ams1013@gmail.com>
2016-01-30 17:43:31 +0200

    change menu, not final

--- help.lua
+++ help.lua
@@ -46,10 +46,15 @@ function scene:create( event )
     -- setup a page background, really not that important though composer
     -- crashes out if there isn't a display object in the view.
     --
-    local background = display.newRect( 0, 0, 570, 360 )
-    background.x = display.contentCenterX
-    background.y = display.contentCenterY
-    sceneGroup:insert(background)
+    local screen_adjustment = 0.4
+    --local background = display.newRect( 0, 0, 570, 360 )
+    local background = display.newImage("images/background3.jpg",true)
+    background.xScale = (screen_adjustment  * background.contentWidth)/background.contentWidth
+    background.yScale = background.xScale
+    background.x = display.contentWidth / 2
+    background.y = display.contentHeight / 2
+    background.alpha = 0.1
+    sceneGroup:insert( background )
 
     --local title = display.newBitmapText( titleOptions )
     -- local title = display.newText("Prime Ritual", 125, 32, native.systemFontBold, 32)
